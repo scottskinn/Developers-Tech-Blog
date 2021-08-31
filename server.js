2a8b1e5b9e50b3ef9b250cb9dba5332211253c9b
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-08-31 12:03:23 -0400

    initial files and packages

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// const mySecret = process.env.DB_Secret;
+
+const sess = {
+  sercet: 'Super secret secret',
+  cookie: {},
+  resave: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session({secret: 'Super secret secret'}, sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+// handlebars engine 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// turn on routes
+app.use(require('./controllers/'));
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
