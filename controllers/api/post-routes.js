be4681fbf5f630af183ea1b03c77a777402057ba
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-09-10 12:29:51 -0400

    server updates

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
-// const { upvote } = require('../../models/Post');
 const withAuth = require('../../utils/auth');
 
 // get all users
